757063c2ffaabe52ad335106f2392a648ec635e8
Josh Reichman <jreichma@fastenal.com>
2022-07-27 14:47:23 -0400

    Directives

--- 1. Getting Started/Exercises/my-first-app/src/app/servers/servers.component.ts
+++ 1. Getting Started/Exercises/my-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,9 @@ export class ServersComponent implements OnInit {
     allowNewServer = false;
     serverCreationStatus = 'No server was created!';
     serverName = 'test server';
+    serverCreated = false;
+    servers = ['Test Server', 'Test Server 2'];
+
     constructor() {
         setTimeout(() => {
             this.allowNewServer = true
@@ -22,6 +25,8 @@ export class ServersComponent implements OnInit {
     }
 
     onCreateServer(){
+        this.serverCreated = true;
+        this.servers.push(this.serverName);
         this.serverCreationStatus = 'Server was created';
     }
 
