7d22db35457fcf41e81dccf700c847cd5d6a5ad2
Josh Reichman <jreichma@fastenal.com>
2022-08-02 14:15:42 -0400

    Assignment 3 completed

--- 2. The Basics/Exercises/basics-assignment-3/src/app/app.component.ts
+++ 2. The Basics/Exercises/basics-assignment-3/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  logs = [];
+  showText = false;
+
+  newLog() {
+    this.showText = !this.showText;
+    this.logs.push(this.logs.length+1);
+  }
+
+  changeColor() {
+    return this.logs.length > 4 ? 'blue' : '';
+  }
 }
