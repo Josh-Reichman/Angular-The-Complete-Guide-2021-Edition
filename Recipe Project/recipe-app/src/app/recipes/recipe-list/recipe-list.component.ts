a44667f8a3a14b2c4e1e82d4c5b1fe314adb4b90
Josh Reichman <jreichma@fastenal.com>
2022-08-15 16:17:16 -0400

    Recipe item, ingredients added

--- Recipe Project/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe Project/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Steak','Ribeye steak',
+      'https://www.carriesexperimentalkitchen.com/wp-content/uploads/2016/04/Tuscan.Style-Grilled-Rib-Eye-Steak3-256x256.jpg'),
+    new Recipe('Chicken','Fried Chicken',
+      'https://redirect.bigoven.com/pics/rs/256/chicken-fried-chicken-13.jpg')
+  ]
 
 }
